87b558cafea29c37c05fc5bf3c17b2b102b0d2d4
SvetlanaDmitrieva <98457743+SvetlanaDmitrieva@users.noreply.github.com>
2024-09-30 15:45:50 +0300

    Update module14_01.py

--- module14_01.py
+++ module14_01.py
@@ -1,3 +1,35 @@
+# Создайте файл базы данных not_telegram.db и подключитесь к ней, используя встроенную библиотеку sqlite3.
+# Создайте объект курсора и выполните следующие действия при помощи SQL запросов:
+# Создайте таблицу Users, если она ещё не создана. В этой таблице должны присутствовать следующие поля:
+# id - целое число, первичный ключ
+# username - текст (не пустой)
+# email - текст (не пустой)
+# age - целое число
+# balance - целое число (не пустой)
+# Заполните её 10 записями:
+# User1, example1@gmail.com, 10, 1000
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 1000
+# ...
+# User10, example10@gmail.com, 100, 1000
+# Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+# User1, example1@gmail.com, 10, 500
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 500
+# ...
+# User10, example10@gmail.com, 100, 1000
+# Удалите каждую 3ую запись в таблице начиная с 1ой:
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 500
+# User5, example5@gmail.com, 50, 500
+# ...
+# User9, example9@gmail.com, 90, 500
+# 
+# Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и 
+# выведите их в консоль в следующем формате (без id):
+# Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
+
+
 import sqlite3
 
 connection = sqlite3.connect('not_telegram.db')
