69ea49f2aa3c276acb9f68962cd06e4fc5bb7b3b
SvetlanaDmitrieva <98457743+SvetlanaDmitrieva@users.noreply.github.com>
2024-10-02 21:20:21 +0300

    Update module13_3.py

--- module13_3.py
+++ module13_3.py
@@ -2,6 +2,8 @@
 # start(message) - печатает строку в консоли 'Привет! Я бот помогающий твоему здоровью.' . Запускается только когда написана команда '/start' в чате с ботом. (используйте соответствующий декоратор)
 # all_massages(message) - печатает строку в консоли 'Введите команду /start, чтобы начать общение.'. Запускается при любом обращении не описанном ранее. (используйте соответствующий декоратор)
 # Запустите ваш Telegram-бот и проверьте его на работоспособность.
+# Измените функции start и all_messages так, чтобы вместо вывода в консоль строки отправлялись в чате телеграм.
+# Запустите ваш Telegram-бот и проверьте его на работоспособность.
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -12,21 +14,13 @@ bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-# @dp.message_handler(text=['Urban','ff'])
-# async def urban_message(message):
-#     print("Urban message")
-#     await message.answer("Urban message")
-
-
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    # print('Привет! Я бот помогающий твоему здоровью.')
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_message(message):
-    # print("Введите команду /start, чтобы начать общение.")
     await message.answer("Введите команду /start, чтобы начать общение.")
 
 
